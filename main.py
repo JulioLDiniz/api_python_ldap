9008f4e764f44f07ea157b6624ab53c73c2e0a9f
Julio Liarte Diniz <julio.diniz4@gmail.com>
2025-01-05 23:32:06 -0300

    Criado função para geração do token JWT

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
 import ldap
-import sys
-from typing import Union
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 from classes.usuarios_input import UsuarioInput
+from jose import jwt, JWTError
+from fastapi import status
+from fastapi.exceptions import HTTPException
+from passlib.context import CryptContext
+from datetime import datetime
+from decouple import config
+from datetime import timedelta
+
+SECRET_KEY = config('SECRET_KEY')
+ALGORITHM = config('ALGORITHM')
+
+crypt_context = CryptContext(schemes=['sha256_crypt'])
 
 app = FastAPI()
+
 
 @app.get('/')
 def read_root():
     return {'Hello':'World'}
 
+@app.post('/auth2')
+def user_login():
+    #VERIFICAR SE USUÁRIO EXISTE
+    #FOO
+    usuario = 'euler'
+    senha = crypt_context.hash('password')
+    
+
+    if usuario is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Credenciais inválidas!'
+        )
+    if not crypt_context.verify('password', senha):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Credenciais inválidas!'
+        )
+    
+    exp = datetime.now() + timedelta(minutes=30)
+
+    payload = {
+        'sub' : usuario,
+        'exp' : exp
+    }
+
+    access_token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+    return {
+        'access_token': access_token,
+        'exp': exp
+     }
+    
 
 @app.post('/auth')
 def acesso_ldap(inputs: UsuarioInput) -> str:
@@ -23,8 +68,19 @@ def acesso_ldap(inputs: UsuarioInput) -> str:
     except ldap.SERVER_DOWN:
         return "Não foi possível se conectar com o servidor LDAP!"
     try:
-        texto = ldap_conn.simple_bind_s(f'uid={inputs.usuario},dc=example,dc=com', f'{inputs.senha}')
-        return 'Usuário conectado com sucesso!'
+        retorno = ldap_conn.simple_bind_s(f'uid={inputs.usuario},dc=example,dc=com', f'{inputs.senha}')
+        if(retorno):
+            return 'Usuário conectado com sucesso!'
+        else:
+            return 'Usuário não encontrado'
     except ldap.INVALID_CREDENTIALS:
         return 'This password is incorrect!'
+
+
+# if __name__ == '__main__':
+#     uvicorn.run(app, port=8000)
+
+
+
+
 
